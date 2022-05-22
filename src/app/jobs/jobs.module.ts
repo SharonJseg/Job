7bcd8d3c1bcdf7ce300e599defbfb21f858f5b7d
SharonJseg <sharon.j.segal@gmail.com>
2022-05-22 04:36:34 +0300

    added job details component

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobsComponent } from './jobs.component';
 import { JobItemComponent } from './job-item/job-item.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
 
 @NgModule({
-  declarations: [JobsComponent, JobItemComponent],
+  declarations: [JobsComponent, JobItemComponent, JobDetailsComponent],
   imports: [CommonModule],
   exports: [JobsComponent],
 })
