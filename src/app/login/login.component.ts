2ad0b53dcca2053ae623e8ef6e47ba5896d53d19
SharonJseg <sharon.j.segal@gmail.com>
2022-05-28 12:07:40 +0300

    added username to job service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { JobService } from '../services/job.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,11 @@ export class LoginComponent implements OnInit {
     password: [undefined, this.passwordValidators],
   });
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private jobService: JobService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -25,6 +30,7 @@ export class LoginComponent implements OnInit {
     console.log(this.formGroup.value);
     console.log('valid', this.formGroup.valid);
     if (this.formGroup.valid) {
+      this.jobService.setUsername(this.formGroup.value.username);
       this.router.navigate(['/jobs']);
     }
   }
